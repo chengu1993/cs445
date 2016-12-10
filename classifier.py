d07d7f368a3741e9f9984504d20d1ed9600d17d7
Zijun Tang <zijun@Zijuns-MBP.wireless.yale.internal>
2016-12-10 15:10:24 -0500

    plot decision tree and add PCA

--- classifier.py
+++ classifier.py
@@ -9,55 +9,113 @@ import pydotplus
 #from IPython.display import Image
 #import pickle
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import decomposition
 
 # import data
-raw_data = pd.read_csv('modified_data.csv', header=0)
+raw_data = pd.read_csv('out.csv', header=0)
 
 # target data set
 target = raw_data['FGM']
 
 # train data set
 train_data = raw_data[['SHOT_DIST', 'FINAL_MARGIN', 'PERIOD',
-                       'SHOT_CLOCK', 'DRIBBLES', 'CLOSE_DEF_DIST']]
+                       'SHOT_CLOCK', 'DRIBBLES', 'CLOSE_DEF_DIST', 'DEFENSE_LEVEL', 'OFFENSE_LEVEL']]
 
-# SVM Classifier definition
-svm_clf = svm.SVC()
+# PCA data
+X = raw_data[['SHOT_DIST', 'FINAL_MARGIN', 'PERIOD',
+              'SHOT_CLOCK', 'DRIBBLES', 'CLOSE_DEF_DIST',
+              'SHOT_NUMBER', 'TOUCH_TIME', 'PTS_TYPE', 'DEFENSE_LEVEL', 'OFFENSE_LEVEL']]
+pca = decomposition.PCA(n_components=6)
+pca.fit(X)
+X = pca.transform(X)
 
-# Test svm classifier model
-svm_scores = cross_val_score(svm_clf, train_data, target, cv=10)
+for data in [X, train_data]:
+    # SVM Classifier definition
+    svm_clf = svm.SVC(C=2.0, kernel='rbf')
 
-print("Score of SVM:")
-print(svm_scores)
-print(svm_scores.mean())
+    # Test svm classifier model
+    svm_scores = cross_val_score(svm_clf, data, target, cv=10)
 
-# Decision tree classifier definition
-tree_clf = tree.DecisionTreeClassifier()
+    print("Score of SVM with RBF:")
+    print(svm_scores)
+    print(svm_scores.mean())
 
-# Test decision tree classifier model
-tree_scores = cross_val_score(tree_clf, train_data, target, cv=10)
+    # SVM Classifier definition
+    svm_poly_clf = svm.SVC(C=2.0, kernel='poly')
 
-print("Score of Decision Tree:")
-print(tree_scores)
-print(tree_scores.mean())
+    # Test svm classifier model
+    svm_poly_scores = cross_val_score(svm_poly_clf, data, target, cv=10)
 
-# Naive Bayes Classifier definition
-gnb = GaussianNB()
+    print("Score of SVM with RBF:")
+    print(svm_poly_scores)
+    print(svm_poly_scores.mean())
 
-# Test Naive Bayes classifier model
-gnb_scores = cross_val_score(gnb, train_data, target, cv=10)
+    # Decision tree classifier definition
+    tree_clf = tree.DecisionTreeClassifier()
 
-print("Score of Naive Bayes:")
-print(gnb_scores)
-print(gnb_scores.mean())
+    # Test decision tree classifier model
+    tree_scores = cross_val_score(tree_clf, data, target, cv=10)
 
-rf_clf = RandomForestClassifier()
-rf_scores = cross_val_score(rf_clf, train_data, target, cv=10)
+    print("Score of Decision Tree:")
+    print(tree_scores)
+    print(tree_scores.mean())
 
-print("Score of Random Forest:")
-print(rf_scores)
-print(rf_scores.mean())
+    # Naive Bayes Classifier definition
+    gnb = GaussianNB()
+
+    # Test Naive Bayes classifier model
+    gnb_scores = cross_val_score(gnb, data, target, cv=10)
+
+    print("Score of Naive Bayes:")
+    print(gnb_scores)
+    print(gnb_scores.mean())
+
+    rf_clf = RandomForestClassifier()
+    rf_scores = cross_val_score(rf_clf, data, target, cv=10)
+
+    print("Score of Random Forest:")
+    print(rf_scores)
+    print(rf_scores.mean())
 
 
+# # SVM Classifier definition
+# svm_clf = svm.SVC()
+#
+# # Test svm classifier model
+# svm_scores = cross_val_score(svm_clf, train_data, target, cv=10)
+#
+# print("Score of SVM:")
+# print(svm_scores)
+# print(svm_scores.mean())
+#
+# # Decision tree classifier definition
+# tree_clf = tree.DecisionTreeClassifier()
+#
+# # Test decision tree classifier model
+# tree_scores = cross_val_score(tree_clf, train_data, target, cv=10)
+#
+# print("Score of Decision Tree:")
+# print(tree_scores)
+# print(tree_scores.mean())
+#
+# # Naive Bayes Classifier definition
+# gnb = GaussianNB()
+#
+# # Test Naive Bayes classifier model
+# gnb_scores = cross_val_score(gnb, train_data, target, cv=10)
+#
+# print("Score of Naive Bayes:")
+# print(gnb_scores)
+# print(gnb_scores.mean())
+#
+# rf_clf = RandomForestClassifier()
+# rf_scores = cross_val_score(rf_clf, train_data, target, cv=10)
+#
+# print("Score of Random Forest:")
+# print(rf_scores)
+# print(rf_scores.mean())
+#
+#
 tree_clf = tree_clf.fit(train_data, target)
 dot_data = tree.export_graphviz(tree_clf, out_file=None,
                                 feature_names=list(train_data.columns.values),
@@ -65,7 +123,15 @@ dot_data = tree.export_graphviz(tree_clf, out_file=None,
                                 filled=True, rounded=True,
                                 special_characters=True)
 graph = pydotplus.graph_from_dot_data(dot_data)
-graph.write_pdf("decision_tree.pdf")
+graph.write_pdf("decision_tree_data.pdf")
+
+tree_clf = tree_clf.fit(X, target)
+dot_data = tree.export_graphviz(tree_clf, out_file=None,
+                                class_names=["missed", "made"],
+                                filled=True, rounded=True,
+                                special_characters=True)
+graph = pydotplus.graph_from_dot_data(dot_data)
+graph.write_pdf("decision_tree_PCA.pdf")
 
 
 # Test result
